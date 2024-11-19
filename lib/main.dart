a22fcb562808c9eac86e5fc05966fbca5d0a4f9d
Hannah Z <eveline1129@163.com>
2024-11-19 11:02:59 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:expense_tracker_advanced_flutter/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
